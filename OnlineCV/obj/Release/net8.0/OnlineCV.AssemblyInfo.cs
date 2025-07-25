a7689cc2e19ee862581bca9c0441b8a8538795ba
Jordan Mosselson <ST10270629@vcconnect.edu.za>
2025-07-25 11:16:00 +0200

    Online CV

--- OnlineCV/obj/Release/net8.0/OnlineCV.AssemblyInfo.cs
+++ OnlineCV/obj/Release/net8.0/OnlineCV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineCV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082e8ab6fed6d809f2f8c5dce5ea32f291a6097c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cbe4e94cb41d32d60aaba597bbf519dbfc2248d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineCV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineCV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
